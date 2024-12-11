4f841cfb3005b015247007ff4c1fb438d7a5b87a
kvostyc <you@example.com>
2024-12-11 19:42:31 +0100

    traits

--- src/Traits/HasOrders.php
+++ src/Traits/HasOrders.php
@@ -6,6 +6,14 @@ use Kvostyc\PrintForgeStock\Helpers\RequestHelper;
 
 trait HasOrders
 {
+    public function getOrders(string $filters = ""): array
+    {
+        $endpoint = "{$this->apiUrl}/api/remote/v1/orders" . $filters;
+        $response = RequestHelper::get($endpoint, $this->apiKey);
+
+        return $response;
+    }
+
     /**
      * Send an order to the Stock API.
      */
